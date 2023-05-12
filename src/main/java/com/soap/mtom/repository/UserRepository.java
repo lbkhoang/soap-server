7911596631c58da84c5201549dfec4f30cc052d4
lbkhoang <lbkhoang>
2023-05-12 09:03:37 +0700

    update user repo

--- src/main/java/com/soap/mtom/repository/UserRepository.java
+++ src/main/java/com/soap/mtom/repository/UserRepository.java
@@ -19,26 +19,26 @@ public class UserRepository {
     @Value( "${config.path.root}" )
     private String ROOT_PATH;
 
-    private static final Map<Integer, User> USER_MAP = new HashMap<>();
+//    private static final Map<Integer, User> USER_MAP = new HashMap<>();
 
-    @PostConstruct
-    public void init() {
-        User user = new User();
-        ProfilePicture pic = new ProfilePicture();
-        user.setId(1);
-        user.setFirstname("Toni");
-        user.setLastname("Kroos");
-        try {
-            pic.setName(user.getId() + "_" + user.getFirstname() + ".jpeg");
-            DataSource source = new FileDataSource(new File(ROOT_PATH + "upload.jpeg"));
-            pic.setContent(new DataHandler(source));
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        USER_MAP.put(user.getId(), user);
-    }
-
-    public User getUserById(int id) {
-        return USER_MAP.get(id);
-    }
+//    @PostConstruct
+//    public void init() {
+//        User user = new User();
+//        ProfilePicture pic = new ProfilePicture();
+//        user.setId(1);
+//        user.setFirstname("Toni");
+//        user.setLastname("Kroos");
+//        try {
+//            pic.setName(user.getId() + "_" + user.getFirstname() + ".jpeg");
+//            DataSource source = new FileDataSource(new File(ROOT_PATH + "upload.jpeg"));
+//            pic.setContent(new DataHandler(source));
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//        USER_MAP.put(user.getId(), user);
+//    }
+//
+//    public User getUserById(int id) {
+//        return USER_MAP.get(id);
+//    }
 }
